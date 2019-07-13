120721cbf85e0602644b15677b6e584563a53cb1
Mateusz Dudek <mateuszddudek@gmail.com>
2019-07-13 14:28:56 +0200

    jpacontrol

--- src/main/java/taxi2/VisualApp/dbEngine/JPAControl.java
+++ src/main/java/taxi2/VisualApp/dbEngine/JPAControl.java
@@ -1,5 +1,19 @@
 package taxi2.VisualApp.dbEngine;
 
+import Drivers.TaxiDriver;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class JPAControl {
+
+		JPAConnect jpa = new JPAConnect();
+		
+		private ObservableList<TaxiDriver> getData(){
+			return FXCollections.observableArrayList();
+			}
+		
+		private (Date)
+
+	}
 
 }
