f29e59835a1c0ed815f44829ed1ae6e660a1afd2
Henrik Kiepe <hkiepe@inkontor.com>
2019-07-13 12:29:43 +0200

    add Taxi Drivers Class

--- src/main/java/Drivers/taxiDriver.java
+++ src/main/java/Drivers/taxiDriver.java
@@ -1,5 +1,50 @@
 package Drivers;
 
-public class taxiDriver {
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+public class taxiDriver {
+	 @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private int id;
+
+	    @Column(name = "taxidriver")
+	    private String taxiDriver;
+	    
+	    @Column(name = "date")
+	    private String driverDate;
+
+	    @Column(name = "status")
+	    private boolean isAvailable;
+
+	    public String getDriverDate() {
+			return driverDate;
+		}
+
+		public void setDriverDate(String driverDate) {
+			this.driverDate = driverDate;
+		}
+
+		public boolean isDriverStatus() {
+			return isAvailable;
+		}
+
+		public void setDriverStatus(boolean driverStatus) {
+			this.isAvailable = driverStatus;
+		}
+
+		public int getId() {
+			return id;
+		}
+
+		public String getTaxiDriver() {
+			return taxiDriver;
+		}
+
+//		@Override
+//	    public String toString() {
+//	        return id + "|" + userName + "|" + userAge;
+//	    }
 }
