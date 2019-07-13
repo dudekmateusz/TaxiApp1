e7d155f4796b00b58e3657bb603044700a289b0d
Marcin Jurak <marcin.jurak1@gmail.com>
2019-07-13 14:09:05 +0200

    gotowe JPAConnect

--- src/main/java/taxi2/VisualApp/dbEngine/JPAConnect.java
+++ src/main/java/taxi2/VisualApp/dbEngine/JPAConnect.java
@@ -9,35 +9,27 @@ import javax.persistence.TypedQuery;
 import Drivers.TaxiDriver;
 
 public class JPAConnect {
-	
-	private EntityManager em = Persistence.createEntityManagerFactory("codeme_taxi2").createEntityManager();
 
-	public static void main(String[] args) {
-		
-		TaxiDriver user = new TaxiDriver();
-		
-		
-	}
-		
-		public List<TaxiDriver> getList() {
-			TypedQuery<TaxiDriver> q = em.createQuery("SELECT u FROM TaxiDriver u", TaxiDriver.class);
-			List<TaxiDriver> res =  q.getResultList();
-			return res;
-		}
-		
-		public void addToDatabase() {
-			em.getTransaction().begin();
-			try {
-				em.persist(user);
-				em.getTransaction().commit();
-			} catch(Exception e) {
-				em.getTransaction().rollback();
-			}
-			
-		}
-		
-		
+	private EntityManager em;
 
+	public JPAConnect() {
+		this.em = Persistence.createEntityManagerFactory("codeme_taxi2").createEntityManager();
 	}
 
+	public List<TaxiDriver> getList() {
+		TypedQuery<TaxiDriver> q = em.createQuery("SELECT u FROM TaxiDriver u", TaxiDriver.class);
+		List<TaxiDriver> res = q.getResultList();
+		return res;
+	}
+
+	public void addToDatabase(TaxiDriver user) {
+		em.getTransaction().begin();
+		try {
+			em.persist(user);
+			em.getTransaction().commit();
+		} catch (Exception e) {
+			em.getTransaction().rollback();
+		}
+	}
+}
 
